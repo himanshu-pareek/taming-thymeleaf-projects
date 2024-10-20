5f907862ce8cae1e0a272fb25facd832f57e7a36
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-10-20 20:12:42 +0530

    Adds dummy pagination

--- thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/infrastructure/web/WebMvcConfiguration.java
+++ thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/infrastructure/web/WebMvcConfiguration.java
@@ -2,11 +2,13 @@ package dev.javarush.taming_thymeleaf.thyme_wizards.infrastructure.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -26,5 +28,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    @RequestScope
+    public ServletUriComponentsBuilder uriBuilder() {
+        return ServletUriComponentsBuilder.fromCurrentRequest();
     }
 }
