b9e7c633e5c6a9559fc714ee84eeeb4bbb172470
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-11-02 22:01:39 +0530

    Adds delete method to delete user

--- thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/user/web/UserController.java
+++ thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/user/web/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -102,7 +103,7 @@ public class UserController {
         throw new RuntimeException("This is a fake exception for testing");
     }
 
-    @PostMapping("{id}/delete")
+    @DeleteMapping("{id}")
     public String deleteUser(@PathVariable("id") UserId userId) {
         userService.deleteUser(userId);
         return "redirect:/users";
