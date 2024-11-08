6ccef99c151ea25c0312e7206ace07a807c79be3
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-11-08 14:49:02 +0530

    Adds roles based configuration for endpoints

--- thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/infrastructure/security/WebSecurityConfiguration.java
+++ thyme-wizards/src/main/java/dev/javarush/taming_thymeleaf/thyme_wizards/infrastructure/security/WebSecurityConfiguration.java
@@ -2,11 +2,15 @@ package dev.javarush.taming_thymeleaf.thyme_wizards.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfiguration {
@@ -23,6 +27,27 @@ public class WebSecurityConfiguration {
         .password(this.passwordEncoder.encode("secure"))
         .roles("USER")
         .build();
-    return new InMemoryUserDetailsManager(user);
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password(this.passwordEncoder.encode("moresecure"))
+        .roles("USER", "ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        authz -> authz.requestMatchers("/users/create").hasRole("ADMIN")
+            .requestMatchers("/users/*/delete").hasRole("ADMIN")
+            .requestMatchers("/users/*/edit").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.GET, "/users/**").hasRole("USER")
+            .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+            .anyRequest().authenticated()
+    )
+        .formLogin(Customizer.withDefaults())
+        .logout(Customizer.withDefaults());
+    return http.build();
   }
 }
